f940e9e0a4a6c8b7b5c5bab101e6d39863a6d54f
Bchenjamin <bzchen@andrew.cmu.edu>
2023-04-09 21:04:08 -0400

    CONTROLLERS 100%

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,6 @@
 class StoresController < ApplicationController
   before_action :set_store, only: [:show, :edit, :update, :destroy]
   before_action :check_login
-  authorize_resource
   
   def index
     @active_stores = Store.active.all
